e711af93a6a103d57534cbef2f7757718dce80d8
motst <84351092+motst@users.noreply.github.com>
2021-05-21 00:49:48 +0300

    Lesson_1

---
+++ lesson_1/area_triangle.rb
@@ -0,0 +1,9 @@
+puts "Enter triangle base"
+base = gets.chomp.to_f
+
+puts "Enter height of the triangle"
+height = gets.chomp.to_f
+
+puts "Area of the triangle #{1.0/2*base*height}"
+
+
---
+++ lesson_1/ideal_weight.rb
@@ -0,0 +1,15 @@
+puts "Enter you name"
+name = gets.chomp
+name.capitalize!
+
+puts "Enter your height"
+height = gets.chomp.to_f
+
+weight = (height-110)*1.15
+
+if weight > 0
+  puts "#{name},your weight is #{weight}"
+else
+  puts "Your weight already is optimal"
+end
+
