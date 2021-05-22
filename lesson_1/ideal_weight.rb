5e8717893006c8c11f27424409b949020d8f461e
motst <84351092+motst@users.noreply.github.com>
2021-05-22 18:34:27 +0300

    Оновление с устранением замечаний

--- lesson_1/ideal_weight.rb
+++ lesson_1/ideal_weight.rb
@@ -1,15 +1,15 @@
-puts "Enter you name"
+puts `Enter you name`
 name = gets.chomp
 name.capitalize!
 
-puts "Enter your height"
+puts `Enter your height`
 height = gets.chomp.to_f
 
-weight = (height-110)*1.15
+weight = (height - 110) *1.15
 
 if weight > 0
   puts "#{name},your weight is #{weight}"
 else
-  puts "Your weight already is optimal"
+  puts `Your weight already is optimal`
 end
 
